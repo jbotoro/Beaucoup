7f38d45c622dd2dc6a2d8f505b516bf20a264531
Jordan Black <jblack530@gmail.com>
2019-09-09 17:08:25 -0700

    added app and root jsx files

---
+++ frontend/components/app.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+    Route, Redirect, Switch,
+    Link, HashRouter
+} from 'react-router-dom';
+import LogInFormContainer from './login_form/login_form_container';
+import SignUpFormContainer from './login_form/signup_form_container';
+
+const App = () => (
+    <div>
+        <header>
+            <Link to='/'>
+                <h1> Beaucoup </h1>
+            </Link>
+        </header>
+        <Switch>
+            <AuthRoute path='/login' component={LogInFormContainer} />
+            <AuthRoute path='/signup' component={SignUpFormContainer} />
+        </Switch>
+    </div>
+);
+
+export default App;
