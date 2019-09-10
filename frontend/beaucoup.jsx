bd98ef312642b96453ea2ff4d4af28df6155d866
Jordan Black <jblack530@gmail.com>
2019-09-09 17:32:40 -0700

    filled out user class file, installed redux-thunk and redux-logger

--- frontend/beaucoup.jsx
+++ frontend/beaucoup.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Root from './components/root';
-// import configureStore from './store/store';
+import Root from './components/root';
+import configureStore from './store/store';
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    // const store = configureStore()
+    const store = configureStore()
     const root = document.getElementById("root");
-    ReactDOM.render(<h1> React is working </h1> , root);
+    ReactDOM.render(<Root store={store} /> , root);
 });
