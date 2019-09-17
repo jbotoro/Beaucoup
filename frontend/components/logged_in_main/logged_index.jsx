07310e08bb4804194456661e4ee0f06ff3bda2f5
Jordan Black <jblack530@gmail.com>
2019-09-17 16:38:56 -0700

    started to organize how im going to build components and how they will fit together

--- frontend/components/logged_in_main/logged_index.jsx
+++ frontend/components/logged_in_main/logged_index.jsx
@@ -36,12 +36,14 @@ class loggedInMain extends React.Component {
                                         </div>
                                     </div>
                                 </div>
+                                {/*  this should be a featured component/featured component container */}
                                 <div className='slider MastheadAboveVerticalSliderCollection__vertical-slider'>
                                     <button className='slider__button slider__button--previous slider__button--hidden'>
 
                                     </button>
                                     <div className='slider__list-wrapper'>
                                         <div className='WindowedList'>
+                                            {/* every WindowedListItem should be a featured tile Item */}
                                             <div className='WindowedListItem'>
                                                 <div>
                                                     <div className='HoverSetup VerticalCoverStoryTile'>
@@ -55,15 +57,35 @@ class loggedInMain extends React.Component {
                                                                     <Link className='VerticalCoverStoryTile__playMasthead'>
                                                                         <span className='sr-only'> Start Watching</span>
                                                                         {/* insert tile play thumbnail here */}
-                                                                        <img src="" alt=""/>
+                                                                        <img src="" className='VerticalCoverStoryTile__rollover-image'/>
                                                                     </Link>
                                                                 </div>
-                                                                <div className='VerticalCoverStoryTile__spacer'>
-
+                                                                <div className='VerticalCoverStoryTile__background-container'>
+                                                                    <div className='VerticalCoverStoryTile__wrapper'>
+                                                                        <div className='VerticalCoverStoryTile__base'></div>
+                                                                        {/* insert tile background thumbnail here */}
+                                                                        <img src="" className='VerticalCoverStoryTile__art'/>
+                                                                        <div className='VerticalCoverStoryTile__scrim'></div>
+                                                                    </div>
                                                                 </div>
                                                             </div>
                                                             <div className='VerticalCoverStoryTile__content'>
+                                                                <div className='VerticalCoverStoryTile__details-wrapper'>
+                                                                    <div className='VerticalCoverStoryTile__details'>
+                                                                        <Link className='VerticalCoverStoryTile__title VerticalCoverStoryTile__title--clickable '></Link>
+                                                                        <ul className='VerticalCoverStoryTile__metadata'>
+                                                                            <li className='VerticalCoverStoryTile__meta'> Season and episode info here</li>
+                                                                        </ul>
+                                                                        <div className='VerticalCoverStoryTile__hoverData'>
+                                                                            <div className='VerticalCoverStoryTile__description'>
 
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                                <div className='VerticalCoverStoryTile__details-item'>
+
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
